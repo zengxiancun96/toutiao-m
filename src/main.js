d2fe5461a8f3c2c6085524501aae8d596569a9bb
曾宪存 <8523179+zeng-xiancun@user.noreply.gitee.com>
2021-01-15 16:42:35 +0800

    cs

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
+// 核心组件库
+import Vant from 'vant';
+// 全局样式
+import 'vant/lib/index.css';
+
+// 注册组件库
+Vue.use(Vant);
 
 Vue.config.productionTip = false
 
